57188cd0387abe63348a1c1805cdf2db9f85c89c
qcryptly <qcryptly@gmail.com>
2019-02-24 23:30:19 -0700

    :new: :bug: Fix configure and cmake scripts to reliably build in docker
    Problem:
    - There was a bug where configure was not properly changing back to the normal directory
    and finishing the build.
    - There was not functionality for toolchains
    
    Solution:
    - Fix configure
    - Add toolchains

--- include/fat.hxx
+++ include/fat.hxx
@@ -50,12 +50,14 @@ struct boot_sector_s {
 };
 
 inline namespace v32 {
+template<auto bytes_per_section = 512>
+using file_info_t = std::uint8_t[];
 using fat_size32_t = std::uint32_t;
-using ext_flags_t = std::uint16_t;
+using ext_flags_t = std::uint8_t[2];
 using version_t = std::uint16_t;
 using root_cluster_t = std::uint32_t;
 using info_t = std::uint16_t;
-using boot_sector_t = std::uint16_t;
+using backup_boot_sector_t = std::uint16_t;
 using reserved_0_t = std::uint8_t[12];
 using drive_number_t = std::uint8_t;
 using reserved_1_t = std::uint8_t;
@@ -65,19 +67,34 @@ using volume_label_t = std::uint8_t[11];
 using file_system_type_t = std::uint64_t;
 using na_0_t = std::uint8_t[420];
 using signature_word_t = std::uint8_t[2];
+using fat_entry_t = std::uint32_t;
+
+// Number of sectors for fat
+// Assuming 8 sectors or 1 cluster
+// we get 4098 kb / 32 byte entries possible
+// for our fat table
+constexpr auto default_fat_size = 8;
+
+template <auto byte_size = 4098>
+struct fat_table_s {
+  fat_entry_t reserved[2] = {0x0ffffff8,0x08000000};
+  fat_entry_t root = 2;
+  fat_entry_t rest[byte_size-sizeof(reserved)-sizeof(root)]{0};
+};
+
 template <auto sector_size> using na_1_t = std::uint8_t[sector_size - 512];
 // bytes = 256MB
 template <auto fat_size_in_bytes = 268435456, auto sector_size = 512>
 struct bios_param_block32_t {
   bios_param_block_t<fat_size_in_bytes, sector_size> bpb{};
-  fat_size32_t fatsize_32{fat_size_in_bytes / sector_size};
-  ext_flags_t ext_flags{0};
+  fat_size32_t fatsize_32{default_fat_size};
+  ext_flags_t ext_flags{0x64 | 0x01, 0x00};
   version_t version{0};
   root_cluster_t root_cluster{2};
   info_t info{1};
-  boot_sector_t boot_sector{0};
+  backup_boot_sector_t backup_boot_sector{2};
   // First 8 sectors are used for first cluster
-  reserved_0_t reserved_0 = {8};
+  reserved_0_t reserved_0 = {0};
   drive_number_t drive_number{0x13};
   reserved_1_t reserved_1{0};
   extended_boot_signature_t extended_boot_signature{0x29};
--- src/fat.cxx
+++ src/fat.cxx
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <fstream>
 
 #include "fat.hxx"
 
 int main() {
   fat::v32::boot_sector_32_s <>bs32{};
-  std::cin.read(reinterpret_cast<char *>(&bs32),sizeof(bs32));
-  std::cout << std::string{reinterpret_cast<char *>(bs32.oem_name)};
+ // fat::v32::boot_sector_32_s <>bs32_default{};
+ // fat::v32::file_info_t <>info;
+  // std::cin.read(reinterpret_cast<char *>(&bs32),sizeof(bs32));
+ // std::cin.read(reinterpret_cast<char *>(&info),sizeof(info));
+  // Set these as defaults
+//  bs32..sectors_per_cluster = bs32_default..sectors_per_cluster;
+//  bs32..number_of_file_allocation_tables = bs32_default..number_of_file_allocation_tables;
+//  bs32..media_type = bs32_default..media_type;
+//  bs32..fatsize_32 = bs32_default..fatsize_32;
+//  bs32..root_cluster = bs32_default..root_cluster;
+//  bs32..info = bs32_default..info;
+//  bs32..backup_boot_sector = bs32_default..backup_boot_sector;
+//  
+//  std::cout << std::string{reinterpret_cast<char *>(bs32.oem_name)};
 } 
